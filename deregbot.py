30e7facdb6cf6b549ae5b9604b4bcc1368c4e23e
Patrick Di Justo <patrick@dijusto.com>
2018-09-21 15:47:23 -0400

    Second go

--- deregbot.py
+++ deregbot.py
@@ -4,10 +4,7 @@ import time
 import csv
 from settings import *
 import datetime
-
-# import requests
-# import urllib
-# import urllib2
+import os
 import wget
 
 
@@ -47,68 +44,7 @@ for row in csv_file:
 	# print(response)
 	api.PostUpdate(status=row[0], media='image.jpg')
 	time.sleep(60)
+	os.remove("image.jpg")
 	
 	
-	
-	
-	
-	
-	
-	
-	# print("using requests.get")
-	# image_url = row[1] ##"https://www.python.org/static/community_logos/python-logo-master-v3-TM.png"
-  
-	# # URL of the image to be downloaded is defined as image_url 
-	# r = requests.get(image_url) # create HTTP response object 
-  
-	# # send a HTTP request to the server and save 
-	# # the HTTP response in a response object called r 
-	# with open("image2.jpg",'wb') as f: 
-  
-    # # Saving received content as a png file in 
-    # # binary format 
-  
-    # # write the contents of the response (r.content) 
-    # # to a new file in binary mode. 
-		# f.write(r.content) 
-	
-
-	# ## New version
-	# # print('Beginning file download with urllib2...')
-
-	# # url = row[1]
-	# # urllib.request.urlretrieve(url, 'image3.jpg') 
-	
-	
-	# ## New version
-
-
-	# print("using urllib2")
-	# filedata = urllib2.urlopen(row[1])  
-	# datatowrite = filedata.read()
-
-	# with open('image4.jpg', 'wb') as f:  
-		# f.write(datatowrite)
-			
-
-	# print('Beginning file download with requests')
-
-	# url = row[1]
-	# r = requests.get(url)
-
-	# with open('image5.jpg', 'wb') as f:  
-		# f.write(r.content)
-
-	# # Retrieve HTTP meta-data
-	# print(r.status_code)  
-	# print(r.headers['content-type'])  
-	# print(r.encoding)  
-	
-	
-	
-
-	# print('Beginning file download with wget module')
-
-	# ###url = 'http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg'  
-	# wget.download(url, 'image6.jpg')  
 	
